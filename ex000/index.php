d84cba9128c37c2a0e26316a2671eb31fff471d7
Thais Amanda <thaisamandaestacio@gmail.com>
2026-01-20 14:22:14 -0300

    comecei a fazer o codigo php

---
+++ ex000/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Me livrando da maldição</title>
+</head>
+<body>
+    <h1>
+        <?php 
+        echo "Olá, mundo! \u{1F30E}"; // echo ou print mesma coisa esse \u(1F30E) é para colocar o emoji do mundo dentro do \u(🌍) igual o windows + . faz vc pega esse codigo no emojipedia
+        ?>
+    </h1>
+    <p>Vamos tentar nos livrar da maldição.</p>
+</body>
+</html>
